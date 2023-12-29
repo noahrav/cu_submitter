ea83e42ce0d6cca89aab357e0df3e3eb5aee2a91
Noah Ravaux <noah.ravaux@epita.fr>
2023-12-29 23:12:03 +0100

    date logs ++

--- src/utils/error.cpp
+++ src/utils/error.cpp
@@ -1,5 +1,21 @@
 #include "error.h"
 
+inline std::string digit_to_string(int digit) {
+    std::string str;
+    if (digit < 10) {
+        str += '0';
+    }
+
+    return str + std::to_string(digit);
+}
+
 void error(const std::string& message) {
-    std::cerr << "Error: " << message << '\n';
+    auto now = time(nullptr);
+    const auto date = localtime(&now);
+
+    std::string date_string = "[" + digit_to_string(date->tm_mday) + "/" + digit_to_string(date->tm_mon + 1) + "/" + std::to_string(date->tm_year + 1900);
+    date_string += " - " + digit_to_string(date->tm_hour) + ":" + digit_to_string(date->tm_min) + ":" + digit_to_string(date->tm_sec) + "] ";
+
+
+    std::cerr << date_string + "Error: " + message << '\n';
 }
--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -1,11 +1,20 @@
 #include "log.h"
 
+inline std::string digit_to_string(int digit) {
+    std::string str;
+    if (digit < 10) {
+        str += '0';
+    }
+
+    return str + std::to_string(digit);
+}
+
 void log(const std::string& message) {
     auto now = time(nullptr);
     const auto date = localtime(&now);
 
-    std::string date_string = "[" + std::to_string(date->tm_mday) + "/" + std::to_string(date->tm_mon + 1) + "/" + std::to_string(date->tm_year + 1900);
-    date_string += " - " + std::to_string(date->tm_hour) + ":" + std::to_string(date->tm_min) + ":" + std::to_string(date->tm_sec) + "] ";
+    std::string date_string = "[" + digit_to_string(date->tm_mday) + "/" + digit_to_string(date->tm_mon + 1) + "/" + std::to_string(date->tm_year + 1900);
+    date_string += " - " + digit_to_string(date->tm_hour) + ":" + digit_to_string(date->tm_min) + ":" + digit_to_string(date->tm_sec) + "] ";
 
     std::cout << date_string + message << '\n';
 }
