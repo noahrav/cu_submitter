ca101b5f987e119867ccd0262abdc685707e5c2c
Noah Ravaux <noah.ravaux@epita.fr>
2023-12-29 23:07:29 +0100

    date logs

--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -1,5 +1,11 @@
 #include "log.h"
 
 void log(const std::string& message) {
-    std::cout << " - " << message << '\n';
+    auto now = time(nullptr);
+    const auto date = localtime(&now);
+
+    std::string date_string = "[" + std::to_string(date->tm_mday) + "/" + std::to_string(date->tm_mon + 1) + "/" + std::to_string(date->tm_year + 1900);
+    date_string += " - " + std::to_string(date->tm_hour) + ":" + std::to_string(date->tm_min) + ":" + std::to_string(date->tm_sec) + "] ";
+
+    std::cout << date_string + message << '\n';
 }
